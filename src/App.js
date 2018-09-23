74559108d4fd583c4d667cb071f6328fd49ba4d0
saqibm128 <saqibm128@gmail.com>
2018-09-22 20:40:10 -0400

    removed more stuff, starting up with cool menu!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import BootstrapScript from './components/BootstrapScripts.js'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <BootstrapScript />
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Mohammed Saqib</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
