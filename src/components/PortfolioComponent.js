e6e61dc8e0b4495769d5fdeee83411480e2f34a7
saqibm128 <saqibm128@gmail.com>
2018-09-24 21:11:53 -0400

    moar projects

--- src/components/PortfolioComponent.js
+++ src/components/PortfolioComponent.js
@@ -5,17 +5,38 @@ class PortfolioComponent extends Component {
     super(props)
     this.dataItems = [
       {
+        idName: 'sepsisResearch',
+        url: 'https://github.com/saqibm128/sepsisProject',
+        imgLink: 'img/embc.png',
+        imgHref: 'https://drive.google.com/file/d/1gaoJys1nc7knThafesXc7E72D1Ly-QTW/view?usp=sharing',
+        name: 'Sepsis Research Project',
+        description: 'A multisemester research project using advanced data cleaning alongside ML techniques'
+      }, {
         idName: 'candyProject',
-        url: 'http://ec2-18-217-140-91.us-east-2.compute.amazonaws.com:8000/',
+        url: 'https://github.com/Saqibm128/Candy-Visualization',
+        imgHref: 'http://ec2-18-217-140-91.us-east-2.compute.amazonaws.com:8000/',
         imgLink: 'img/candyProject.png',
         name: 'Candy Visualization',
-        description: 'A multigraph data visualization of survey data of favorite candies.',
+        description: 'A multigraph data visualization of survey data of favorite candies.'
+      }, {
+        idName: 'ticTacToeRobot',
+        url: 'https://github.com/Saqibm128/3790-cozmo',
+        imgLink: 'img/cozmo.jpg',
+        imgHref: 'https://docs.google.com/document/d/1vhKRjn3CHxUjeBYBFbBGabb28uuDLMMKIAsD3rlVdE0/edit?usp=sharing',
+        name: 'Robot Tic Tac Toe',
+        description: 'Programming a simple robot, Cozmo, to play Tic Tac Toe with a human'
       }, {
         idName: 'BHIProject',
         url: 'https://github.com/Saqibm128/BHIProject',
         imgLink: 'img/ebola.png',
         name: 'Ebola Model Simulation',
         description: 'A GUI for modeling the 2014 Ebola epidemic'
+      }, {
+        idName: 'ECG Grapher',
+        url: 'https://github.com/Saqibm128/pqrs-identifier',
+        imgLink: 'img/ptqrs.png',
+        name: 'ECG Grapher, Analyzer',
+        description: 'An implementation of PQRS identifier for ECG signals, in both MatLab and LabView'
       }, {
         idName: 'ultimateTicTacToe',
         url: 'https://github.com/Saqibm128/UltimateTicTacToe',
@@ -24,13 +45,23 @@ class PortfolioComponent extends Component {
         description: 'A JavaFX program for playing a 3x3x3 game of Tic Tac Toe'
       }
     ]
-    this.state = {dataItems: this.dataItems}
+    this.state = {
+      dataItems: this.dataItems
+    }
   }
   render() {
-    var portfolioItemList = this.state.dataItems.map((item, ind) => {
+    var portfolioItemList = []
+    for (var i = 0; i < Math.ceil(this.state.dataItems.length / 3.0); i++) {
 
-      return (<PortfolioItemComponent key={ind} data={item}/>)
-    })
+      var end = i * 3 + 3 < this.state.dataItems.length
+        ? i * 3 + 3
+        : this.state.dataItems.length
+      portfolioItemList.push(
+        <div className='row portfolio'>
+          {this.state.dataItems.slice(i * 3, end).map((item) => <PortfolioItemComponent key={item.idName} data={item}/>)}
+        </div>
+      )
+    }
 
     return (
       <div id='portfolio'>
--- src/components/PortfolioItemComponent.js
+++ src/components/PortfolioItemComponent.js
@@ -6,12 +6,12 @@ class PortfolioItemComponent extends Component {
   }
   render() {
     return (
-      <div id={this.props.data.idName} className='col col-xs-4 portfolio port-item'>
+      <div id={this.props.data.idName} className='col portfolio port-item'>
         <a href={this.props.data.url}>
           <h2>{this.props.data.name}</h2>
         </a>
         <p>{this.props.data.description}</p>
-        <a href={this.props.data.url}><img src={this.props.data.imgLink} className='text-center mx-auto portfolio-image'/></a>
+        <a href={this.props.data.imgHref ? this.props.data.imgHref : this.props.data.url}><img src={this.props.data.imgLink} className='text-center mx-auto portfolio-image'/></a>
       </div>
     )
 
